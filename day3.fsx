1d381bcc47e11ca9637ab997f3e57102746d088d
mikesigs <mikesigsworth@gmail.com>
2017-12-08 14:18:55 -0700

    Day 3 Part 1 complete.

--- day3.fsx
+++ day3.fsx
@@ -1,20 +1,16 @@
 type Coord = (int * int)
-type dir = Up | Down | Left | Right
+type Dir = Up | Down | Left | Right
 let moveUp (c: Coord) (d: int) = fst c, snd c + d
 let moveDown (c: Coord) (d: int) = fst c, snd c - d
 let moveLeft (c: Coord) (d: int) = fst c - d, snd c
 let moveRight (c: Coord) (d: int) = fst c + d, snd c
-let move (dist: int) (dir: dir) (pos: int * int) =
+let move (dist: int) (dir: Dir) (pos: int * int) =
     printfn "dist: %d, dir: %A, pos: %A" dist dir pos
-
-    let newpos = 
-        match dir with
-        | Up -> moveUp pos dist
-        | Down -> moveDown pos dist
-        | Left -> moveLeft pos dist
-        | Right -> moveRight pos dist
-    
-    newpos
+    match dir with
+    | Up -> moveUp pos dist
+    | Down -> moveDown pos dist
+    | Left -> moveLeft pos dist
+    | Right -> moveRight pos dist    
 
 let turnLeft = function
 | Up -> Left
@@ -22,17 +18,24 @@ let turnLeft = function
 | Down -> Right
 | Right -> Up
 
-let turnAround = turnLeft >> turnLeft
+let turnRight = turnLeft >> turnLeft >> turnLeft
 
-let draw max =    
-    let rec step i max dist dir pos =
-        if i > max then pos
+let findTargetPos target =    
+    let rec step i target dist dir pos =
+        if i >= target then    
+            printfn "pos: %A, target: %d, i: %d, dir: %A" pos target i dir
+            move (abs target-i) (turnRight dir) pos
         else 
-            pos
-            |> move dist dir
-            |> move dist (turnLeft dir)
-            |> step (i + dist*2) max (dist+1) (turnAround dir)
+            let newPos = move dist dir pos
+            let newDir = turnLeft dir
+            let newDist = 
+                match newDir with
+                | Left | Right -> dist + 1
+                | Up | Down -> dist
+            
+            step (i + dist) target newDist newDir newPos
 
-    step 1 max 1 Right (0,0)
+    step 1 target 1 Right (0,0)
 
-draw 25
+findTargetPos 289326 
+|> fun (x,y) -> abs x + abs y
