78180e0ff34c728beb0d0bdfbaaf822654195975
mikesigs <mikesigsworth@gmail.com>
2017-12-17 21:43:39 -0700

    Day 4 part 1 and 2 complete

--- day4.fsx
+++ day4.fsx
@@ -1,10 +1,16 @@
+// Return the input with its characters in alphabetical order
+let alphabetize (input: string) : string = input |> Seq.toList |> Seq.sort |> System.String.Concat
+let getWords (input: string) = input.Split(' ') |> Seq.ofArray
+let countTrues s = Seq.fold (fun count e -> if e then count + 1 else count) 0 s
+
+let day4 projection input = (input |> Seq.distinctBy projection |> Seq.length) = (Seq.length input)
+let day4part1: seq<string> -> bool = day4 id
+let day4part2: seq<string> -> bool = day4 alphabetize
+
+let lines = System.IO.File.ReadLines "data/day4.txt"
+
 // Part 1
-let day4 (input: seq<string>) = 
-    (input |> Seq.distinct |> Seq.length) = (Seq.length input)
+lines |> Seq.map (getWords >> day4part1) |> countTrues //=> 337
 
-let split (by: char) (input: string) = input.Split(by) |> Seq.ofArray
-let splitWords = split ' '
-
-System.IO.File.ReadLines "data/day4.txt"
-|> Seq.map (splitWords >> day4) 
-|> Seq.fold (fun count e -> if e then count + 1 else count) 0 //=> 337
+// Part 2
+lines |> Seq.map (getWords >> day4part2) |> countTrues //=> 231
