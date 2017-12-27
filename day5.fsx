56eacd476602b1c0688e108930bd9b4a3c21afa5
mikesigs <mikesigsworth@gmail.com>
2017-12-27 01:03:44 -0700

    Part 2 complete

--- day5.fsx
+++ day5.fsx
@@ -1,14 +1,17 @@
-let rec day5 (c: int) (i: int) (input: array<int>) =
+let rec day5 (c: int) (i: int) (inc: int -> int) (input: array<int>) =
     if (i > input.Length - 1) then c
     else  
         let jmp = input.[i]
         let left = if (i-1 < 0) then [||] else input.[..i-1]
         let right = if (i+1 > input.Length) then [||] else input.[i+1..]
-        day5 (c + 1) (i + jmp) (Array.concat [ left; [| jmp + 1 |]; right ])
+        day5 (c + 1) (i + jmp) inc (Array.concat [ left; [| inc jmp |]; right ])
 
-System.IO.File.ReadAllLines "data/day5.txt" 
-|> Array.map int
-|> day5 0 0
+let input = 
+    System.IO.File.ReadAllLines "data/day5.txt" 
+    |> Array.map int
 
-day5 0 0 [| 1; -1; -2 |]
+// Part 1
+input |> day5 0 0 ((+) 1)
 
+// Part 2
+input |> day5 0 0 (fun x -> if x >= 3 then x - 1 else x + 1)
